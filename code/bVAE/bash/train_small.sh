3e847ffe3b9860558376f17aba3fbd4a0954b1c9
Yair LAKRETZ <yl254115@is153802.intra.cea.fr>
2022-04-19 10:11:28 +0200

    small train bash

---
+++ code/bVAE/bash/train_small.sh
@@ -0,0 +1,18 @@
+BETAS='1 2'
+BATCH_SIZES='64 128'
+LATENT_SIZES='16 32'
+LEARNING_RATES='0.001 0.01'
+
+# NUMBER OF STIMULI
+
+for BETA in $BETAS; do
+    for BATCH_SIZE in $BATCH_SIZES; do
+        for LATENT_SIZE in $LATENT_SIZES; do
+            for LEARNING_RATE in $LEARNING_RATES; do
+                LOG_PATH='logs/log_beta_'$BETA'_latent_size_'$LATENT_SIZE'_batch_size_'$BATCH_SIZE'_learning_rate_'$LEARNING_RATE'.pkl'
+                cmd='python train.py --beta '$BETA' --batch-size '$BATCH_SIZE' --latent-size '$LATENT_SIZE' --learning-rate '$LEARNING_RATE' --log-path '$LOG_PATH' --use-cuda'
+                echo $cmd
+            done
+        done
+    done
+done
